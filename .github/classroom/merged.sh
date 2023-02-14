e14b02167416775effa8c58d857d0104c7ad411b
Simon Grønborg <simon.gronborg@gmail.com>
2023-02-14 13:46:38 +0000

    chore: fix lars's failing merge test

--- .github/classroom/merged.sh
+++ .github/classroom/merged.sh
@@ -2,12 +2,12 @@
 
 echo Looking up if all branches are merged into master
 
-Merged:
+#Merged:
 
 git branch --merged master  | grep -P "father"
 git branch --merged master  | grep -P "lion"
 git branch --merged master  | grep -P "turtle"
 
-Not Merge:
+#Not Merge:
 
 git branch --no-merged master
